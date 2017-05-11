54e55a34c308af004fac685b641d5928eb6662fe
Aravindh-r <aravindhchandran94@gmail.com>
2017-05-11 16:43:45 +0530

    files added

---
+++ Script/edubar.js
@@ -0,0 +1,76 @@
+var margin = {top: 40, right: 150, bottom: 500, left: 150},
+width = 1500 - margin.left - margin.right,
+height = 1500 - margin.top - margin.bottom;
+
+
+// set the ranges
+var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+var y = d3.scale.linear().range([height, 0]);
+
+// define the axis
+var xAxis = d3.svg.axis()
+.scale(x)
+.orient("bottom")
+
+
+var yAxis = d3.svg.axis()
+.scale(y)
+.orient("left")
+.ticks(10);
+
+
+// add the SVG element
+var svg = d3.select("body").append("svg")
+.attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform",
+  "translate(" + margin.left + "," + margin.top + ")");
+
+
+// load the data
+d3.json("../output/Education.json", function(error, data) {
+
+  data.forEach(function(d) {
+    d.Education_catogories = d.Education_catogories;
+    d.Total_population = +d.Total_population;
+  });
+
+// scale the range of the data
+x.domain(data.map(function(d) { return d.Education_catogories; }));
+y.domain([0, d3.max(data, function(d) { return d.Total_population; })]);
+
+// add axis
+svg.append("g")
+.attr("class", "x axis")
+.attr("transform", "translate(0," + height + ")")
+.call(xAxis)
+.selectAll("text")
+.style("text-anchor", "end")
+.attr("dx", "-.8em")
+.attr("dy", "-.55em")
+.attr("transform", "rotate(-90)" );
+
+svg.append("g")
+.attr("class", "y axis")
+.call(yAxis)
+.append("text")
+.attr("transform", "rotate(-90)")
+.attr("y", 5)
+.attr("dy", ".71em")
+.style("text-anchor", "end")
+.text("Literacy");
+
+
+// Add bar chart
+svg.selectAll("bar")
+.data(data)
+.enter().append("rect")
+.attr("class", "bar")
+.attr("x", function(d) { return x(d.Education_catogories); })
+.attr("width", x.rangeBand())
+.attr("y", function(d) { return y(d.Total_population); })
+.attr("height", function(d) { return height - y(d.Total_population); });
+
+});
